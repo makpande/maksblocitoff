4044b860d7443b92a41f000b15bbf01dd371b7a2
Makarand Pande <mak_pande@yahoo.com>
2015-11-11 15:09:59 -0500

    master not uptodate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
-  def index
-   @user = current_user
- end
-
- def update
-   if current_user.update_attributes(user_params)
-     flash[:notice] = "User information updated"
-     redirect_to edit_user_registration_path
-   else
-     flash[:error] = "Invalid user information"
-     redirect_to edit_user_registration_path
-   end
- end
-
- def show
-   @user = current_user
-   @user = User.find(params[:id]) if params[:id]
-   @items = @user.items
-   
+  
  end
 
 
--- db/migrate/20151110222614_add_expiration_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddExpirationToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :expiration, :datetime
-  end
-end
